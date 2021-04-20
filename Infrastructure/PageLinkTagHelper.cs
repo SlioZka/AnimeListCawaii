c6f8e3aca25f145267e80a1923860ee904fcddf4
Serhii Shnitser <sergik.shnitser@gmail.com>
2021-04-20 22:48:07 +0300

    new code

--- Infrastructure/PageLinkTagHelper.cs
+++ Infrastructure/PageLinkTagHelper.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Mvc.Routing;
 using Microsoft.AspNetCore.Mvc.ViewFeatures;
 using Microsoft.AspNetCore.Razor.TagHelpers;
 using AnimeList.Models.ViewModels;
+using System.Collections.Generic;
 
 namespace AnimeList.Infrastructure
 {
@@ -20,6 +21,9 @@ namespace AnimeList.Infrastructure
         public ViewContext ViewContext { get; set; }
         public PagingInfo PageModel { get; set; }
         public string PageAction { get; set; }
+        [HtmlAttributeName(DictionaryAttributePrefix = "page-url-")]
+        public Dictionary<string, object> PageUrlValues { get; set; }
+            = new Dictionary<string, object>();
         public bool PageClassesEnabled { get; set; } = false;
         public string PageClass { get; set; }
         public string PageClassNormal { get; set; }
@@ -31,6 +35,8 @@ namespace AnimeList.Infrastructure
             for (int i = 1; i <= PageModel.TotalPages; i++)
             {
                 TagBuilder tag = new TagBuilder("a");
+                PageUrlValues["productPage"] = i;
+                tag.Attributes["href"] = urlHelper.Action(PageAction, PageUrlValues);
                 tag.Attributes["href"] = urlHelper.Action(PageAction,
                 new { productPage = i });
                 if (PageClassesEnabled)
