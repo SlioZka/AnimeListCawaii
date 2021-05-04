bca32ad9a8e8ebc72a001c92877e9c68b8d6ddcb
Serhii Shnitser <sergik.shnitser@gmail.com>
2021-05-04 20:29:26 +0300

    add navigation menu

---
+++ Components/NavigationMenuViewComponent.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AnimeList.Models;
+
+namespace AnimeList.Components
+{
+    public class NavigationMenuViewComponent: ViewComponent
+    {
+        private IAnimeListRepository repo;
+        public NavigationMenuViewComponent(IAnimeListRepository _repo)
+        {
+            repo = _repo;
+        }
+        public IViewComponentResult Invoke()
+        {
+            return View(repo.Titles
+                .Select(x => x.Category)
+                .Distinct()
+                .OrderBy(x => x));
+        }
+    }
+}
